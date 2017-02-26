1fcc3cb6533d2ff90572f0a989585e6f080e1e0f
zhouyong <zhouyong@chinac.com>
2017-02-26 13:17:53 +0800

    init project

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ssehacker on 2017/2/25.
+ */
+
+import Slider from './src';
+
+export default Slider;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './index.less';
+
+class Slider extends React.Component {
+  
+  render() {
+    const me = this;
+    return (
+      <div className="neo-slider">
+        Slider
+      </div>
+    );
+  }
+}
+
+export default Slider;
