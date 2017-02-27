dee58c31569d09359cbd026536513a904379f998
zhouyong <zhouyong@chinac.com>
2017-02-27 14:36:00 +0800

    Carousel support.

---
+++ src/SliderBlock.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ssehacker on 2017/2/26.
+ */
+import classnames from 'classnames';
+
+class SliderBlock extends React.Component {
+  render() {
+    const { className, ...other} = this.props;
+    return (
+      <div
+        className={classnames({'neo-slider-list-item': true, [`${className}`]: !!className})}
+        {...other}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default SliderBlock;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,70 @@
+import classnames from 'classnames';
 import './index.less';
+import SliderBlock from './SliderBlock';
 
 class Slider extends React.Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
+
+  handleSwitch(dir) {
+    const maxLen = this.props.children.length;
+    let index = this.state.current + dir;
+    if (index < 0) {
+      index += maxLen;
+    }
+
+    if (index >= maxLen) {
+      index -= maxLen;
+    }
+    this.setState({
+      current: index,
+    });
+  }
+
+  renderChildren() {
+    const { children } = this.props;
+    const { current } = this.state;
+    return children.map((item, index) => {
+      if(index===0) {
+        let style = {
+          marginLeft: `${-1 * current * 100}vw`,
+        };
+        return React.cloneElement(item, {style})
+      }
+      return item;
+    });
+  }
+
   render() {
     const me = this;
+    const { handleSwitch } = me;
+    const { className } = this.props;
     return (
-      <div className="neo-slider">
-        Slider
+      <div className={classnames({'neo-slider': true, [`${className}`]: !!className})}>
+        <a
+          className="neo-slider-left"
+          onClick={handleSwitch.bind(me, -1)}
+        >
+          <i className="iconfont icon-left"/>
+        </a>
+        <a
+          className="neo-slider-right"
+          onClick={handleSwitch.bind(me, 1)}
+        >
+          <i className="iconfont icon-right"/>
+        </a>
+        <div className="neo-slider-list">
+          {me.renderChildren()}
+        </div>
       </div>
     );
   }
 }
 
+Slider.SliderBlock = SliderBlock;
 export default Slider;
